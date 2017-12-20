09a6534f9eb97e65886112044e87ea1e05743ee5
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-12-20 23:06:44 +0100

    Add sentry files to bot definitions

--- src/adapters/definitions/CreateBotData.ts
+++ src/adapters/definitions/CreateBotData.ts
@@ -2,4 +2,5 @@ export interface CreateBotData {
 	readonly username: string
 	readonly password: string
 	readonly steamGuardEnabled: boolean
+	readonly sentryFile?: Buffer
 }
--- src/adapters/definitions/UpdateBotData.ts
+++ src/adapters/definitions/UpdateBotData.ts
@@ -2,4 +2,5 @@ export interface UpdateBotData {
 	readonly username?: string
 	readonly password?: string
 	readonly steamGuardEnabled?: boolean
+	readonly sentryFile?: Buffer
 }
